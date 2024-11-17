6f213ca7d0b3c30513ada53a50d763133bfb7c22
danh2006 <107236039+danh2006@users.noreply.github.com>
2024-11-18 03:16:19 +0700

    Update Point.h

--- Point.h
+++ Point.h
@@ -10,6 +10,8 @@ struct Point{
     Point operator+(Point p){ x += p.x, y += p.y; return *this; }
     Point operator-=(Point p){ *this = *this - p; return *this; }
     Point operator+=(Point p){ *this = *this + p; return *this; }
+    bool  operator==(Point p) { return x == p.x && y == p.y; }
+    bool  operator!=(Point p) { return !(*this == p); }
 };
 
 std::istream& operator>>(std::istream& is, Point& p){
@@ -20,4 +22,9 @@ std::ostream& operator<<(std::ostream& os, Point p){
     os << p.x << ' ' << p.y << "\n";
     return os;
 }
+
+auto GetIdx(int x, int y){ 
+    return (x << 3) + y; 
+}
+auto GetIdx (Point p){ return Getidx(p.x, p.y); }
 
