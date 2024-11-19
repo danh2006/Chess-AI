d14738cbe48efd2767aeaa13dcb24a99ca885636
danh2006 <107236039+danh2006@users.noreply.github.com>
2024-11-20 03:35:08 +0700

    Update ChessBoard.cpp

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -1,5 +1,5 @@
 #include<cstdio>
-#include"Piece.h"
+#include"ChessPiece.h"
 
 
 class Board{
@@ -11,12 +11,15 @@ private:
     std::vector<Queen>  wQueen,  bQueen;
     King                wKing,   bKing;
 
-    bool turn;
-
+    bool    turn;
+    int     alpha;
+    int     beta;
+    short   depth;
+    times_t times; 
 
 public:
     void init(){
-        wKing(Point(7, 4)); bKing({0, 4});
+        wKing({7, 4}); bKing({0, 4});
         for(int y = 0; y < 8; ++y){
             wPawn.push_back(Point(6, y));
             bPawn.push_back(Point(1, y));
@@ -30,34 +33,63 @@ public:
         bBishop = {{0, 2}, {0, 5}};
     }
 
-    // auto& select(int piece_value){
-    //     switch(piece_value):
-    //         case Queen:  return turn ? &wQueen  : &bQueen;
-    //         case Rook:   return turn ? &wRook   : &bRook;
-    //         case Bishop: return turn ? &wBishop : &bBishop;
-    //         case Knight: return turn ? &wKnight : &bKnight;
-    //         case Pawn:   return turn ? &wPawn   : &bPawn;
-    //         // case King      
-    // }
+    template<class T> auto& choose(T&& param_one, T&& param_two){
+        return turn == White ? param_one : param_two;
+    }
+    template<class Func, class Ranges, class T> 
+    auto make(Func&& func, Ranges&& range, T&& paramType){
+        return func(range.begin(), range.end(), paramType);
+    }
+    template<class Func, class T> 
+    auto select(Func&& func, int piece_value, bool side, T&& paramType){
+        // [&](){ 
+            switch(piece_value) { 
+                case Queen:  
+                    return make(func, side ? wQueen  : bQueen,  paramType); 
+                case Rook:   
+                    return make(func, side ? wRook   : bRook,   paramType); 
+                case Bishop:  
+                    return make(func, side ? wBishop : bBishop, paramType); 
+                case Knight: 
+                    return make(func, side ? wKnight : bKnight, paramType); 
+                case Pawn:   
+                    return make(func, side ? wPawn   : bPawn,   paramType); 
+                default:     
+                    assert(false); 
+            } 
+        // }() 
+    }
+
+    template<class T> 
+    void change_position_piece(T&& src, T&& dst, Point& srcPosition, Point& dstPosition){
+        
+        auto& pieceMovePosInList = select(find, src, turn, srcPosition);
+             *pieceMovePosInList = dstPosition; 
+
+        if(dst != Empty){
+            auto& pieceCapturePosInList = select(find, dst, !turn, dstPosition);
+            *pieceCapturePosInList = Point(Empty, Empty);
+        }
+
+        src = dst;
+    }
 
     void simulation(std::vector<Point>& command){
-        for(int i = 0; i < command.size(); i += 2){
-            auto& src = ;
-            auto& dst = ;
-            assert(src != EMPTY && dst == EMPTY);
-            src = dst;
-            dst = EMPTY;
-            turn ^= 1;
+        for(int i = 0; i < command.size() && turn ^= 1; i += 2){
+            auto& board = choose(WhiteBoard, BlackBoard);
+            auto& src   = board[GetIdx(comand[i])];
+            auto& dst   = board[GetIdx(command[i + 1])];
+            change_position_piece(&src, &dst, command[i], command[i+1]);
         }
     }
-    
+
     template<class T>
     auto check_move(T&& pieces, Point& position){
         // piece we need to check
         auto piece = *find_if(pieces.begin(), pieces.end(), [&](auto& p){
             return p.position == position;
         }); 
-        return piece.move == false; 
+        return piece.moved == false; 
     }
 
     auto can_kingside_castle(auto&& board, auto& row){ // kiem tra xem co the nhap thanh gan khong
@@ -67,8 +99,8 @@ public:
         }
         // kiem tra xem cac quan co nay da di chuyen chua
         return board[GetIdx(row, 7)] == Rook && board[GetIdx(row, 4)] == King &&
-               checkMove(turn ? wRook : bRook, Point(row, 7)) && 
-               checkMove(turn ? wKing : bKing, Point(row, 4));
+               check_move(turn ? wRook : bRook, Point(row, 7)) && 
+               check_move(turn ? wKing : bKing, Point(row, 4));
     }
 
     auto can_queenside_castle(auto&& board, auto& row){ // kiem tra xem co the nhap thanh xa khong
@@ -78,17 +110,34 @@ public:
         }
         // kiem tra xem cac quan co nay da di chuyen chua
         return board[GetIdx(row, 0)] == Rook && board[GetIdx(row, 4)] == King &&
-               checkMove(turn ? wRook : bRook, Point(row, 0)) && 
-               checkMove(turn ? wKing : bKing, Point(row, 4));
+               check_move(turn ? wRook : bRook, Point(row, 0)) && 
+               check_move(turn ? wKing : bKing, Point(row, 4));
     }
 
     std::vector<Point> castle(){
         auto& board = turn ? WhiteBoard : BlackBoard;
-        auto  row   = turn ? 8 : 1;  
+        auto  row   = turn ? 7 : 0;  
         if(can_kingside_castle(board, row))
 
         if(can_queenside_castle(board, row))
 
+    }
+
+    auto set_depth(int16_t _depth){
+        depth = _depth;
+    } 
+    
+    auto set_times(time_t max_times){
+        times = max_times;
+    }
+
+    auto minimax(){
+        
+    }
+
+    void best_next_move(){
+        auto command = minimax();
+        cout << en_code(command) << endl;
     }
 /*
     std::vector<Point> get_all_next_pos_enemy(){
@@ -140,3 +189,14 @@ public:
         }
     }
 };
+
+int main(){
+
+    auto record = parse();
+    Board board; 
+    board.init();
+    board.simulation(record);
+    board.minimax();
+    board.best_next_move();
+    board.display();
+}
