0e57ba62817fe1185dc513a558637fbdab7ce81a
Duc Anh Le <107236039+danh2006@users.noreply.github.com>
2024-12-12 14:39:21 +0700

    Add files via upload

---
+++ PGN.h
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <cstdint>
+
+
+std::string encode(int16_t position){
+    int x = position / 8, y = position % 8;
+    x = 7 - x;
+    std::string ret_str;
+    ret_str += char(y + 'a');
+    ret_str += char(x + 1 + '0');
+    return ret_str;
+}
+
+
+std::vector<int16_t> parse(){
+    std::vector<int16_t> record;
+    std::string input;
+    while(std::getline(std::cin, input)){
+        if(input == "END") break;
+        assert(input[2] == ' ');
+        auto from = (8 - int(input[1] - '0')) * 8 +  int(input[0] - 'a');
+        auto to   = (8 - int(input[4] - '0')) * 8 +  int(input[3] - 'a');
+        record.emplace_back(from);
+        record.emplace_back(to);
+    }
+    return record;
+}
